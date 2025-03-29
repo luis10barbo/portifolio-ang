85b97c7fa4e53fd5f195ac140ced1778dd220686
luis10barbo <luis10barbo2020@gmail.com>
2025-03-29 16:46:13 -0300

    refactor: Renamed from project-component to project

--- client/src/app/routes/project/project.component.ts
+++ client/src/app/routes/project/project.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-project-component',
   imports: [],
-  templateUrl: './project-component.component.html',
-  styleUrl: './project-component.component.scss'
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.scss'
 })
 export class ProjectComponentComponent {
 
