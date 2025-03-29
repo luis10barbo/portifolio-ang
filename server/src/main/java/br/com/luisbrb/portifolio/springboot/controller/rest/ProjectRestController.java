f259e91976ebe871795c4a59dc92f764791eb333
luis10barbo <luis10barbo2020@gmail.com>
2025-03-29 18:39:13 -0300

    feat: Added routes to get singular project and fixed multi project route

--- server/src/main/java/br/com/luisbrb/portifolio/springboot/controller/rest/ProjectRestController.java
+++ server/src/main/java/br/com/luisbrb/portifolio/springboot/controller/rest/ProjectRestController.java
@@ -1,10 +1,12 @@
 package br.com.luisbrb.portifolio.springboot.controller.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.luisbrb.portifolio.springboot.controller.repositories.ProjectRepository;
@@ -28,22 +30,14 @@ public class ProjectRestController {
         this.skillRepository = skillRepository;
     }
 
-    @GetMapping()
+    @GetMapping("/all")
     Iterable<ProjectEntity> getAll() {
         return projectRepository.findAll();
     }
 
-    @Getter
-    @ToString
-    public static class BodySaveProject {
-        private @NotNull String name;
-        private String description;
-        private String repo;
-        private String website;
-        private String download;
-        private List<SkillEntity> techBack;
-        private List<SkillEntity> techFront;
-        private ProjectStatusEnum projectStatusEnum;
+    @GetMapping("/")
+    Optional<ProjectEntity> get(@RequestParam("id") Long id) {
+        return this.projectRepository.findById(id);
     }
 
     @PostMapping()
