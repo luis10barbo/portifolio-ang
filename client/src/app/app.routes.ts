44ade02df518a0e00f8904ffa94bda7b80825328
luis10barbo <luis10barbo2020@gmail.com>
2025-03-29 18:37:28 -0300

    fix: Fixed url archors and added route to projects

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CriarProjetoComponent } from './routes/admin/criar-projeto/criar-projeto.component';
 import { CriarHabilidadeComponent } from './routes/admin/criar-habilidade/criar-habilidade.component';
 import { AppComponent as HomeComponent } from './routes/home/home.component';
+import { ProjectComponentComponent } from './routes/project/project.component';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,6 @@ export const routes: Routes = [
     },
     {
         path: "proj/:id",
-        component: CriarHabilidadeComponent
+        component: ProjectComponentComponent
     }
 ];
