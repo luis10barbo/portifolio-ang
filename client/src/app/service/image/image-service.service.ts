044d3349ca483ccf726b8a6221f1b664b0fba0c3
luis10barbo <luis10barbo2020@gmail.com>
2025-03-31 06:42:25 -0300

    fix: Allow uploading skill without image

--- client/src/app/service/image/image-service.service.ts
+++ client/src/app/service/image/image-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { enviroment } from '../../../environment';
 import { HttpClient } from '@angular/common/http';
 import { ImageType } from '../../model/imageModel';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,23 @@ export class ImageServiceService {
   constructor(private httpClient: HttpClient) {}
 
   public saveImages(images:({file: File, blob: string} | undefined)[]) {
-    const formData = new FormData();
-    images.forEach((image) => {
-      if (!image) {
+    return new Observable<ImageType[] | undefined>((subscriber) => {
+      if (images.length < 1) {
+        subscriber.next(undefined);
         return;
       }
-      formData.append("image", image.file);
-    });
-    return this.httpClient.post<ImageType[]>(`${enviroment.urlBackend}/image`, formData, {withCredentials: true});
+      const formData = new FormData();
+      images.forEach((image) => {
+        if (!image) {
+          return;
+        }
+        formData.append("image", image.file);
+      });
+      this.httpClient.post<ImageType[]>(`${enviroment.urlBackend}/image`, formData, {withCredentials: true}).subscribe((res) => {
+        subscriber.next(res);
+      });
+    })
+    
   }
 
 
