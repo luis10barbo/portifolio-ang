e75d53fa97c5b5afd72c385a8f942ff1314a1862
luis10barbo <luis10barbo2020@gmail.com>
2025-03-31 05:27:12 -0300

    feat: Added route to edit skill

--- server/src/main/java/br/com/luisbrb/portifolio/springboot/controller/rest/SkillRestController.java
+++ server/src/main/java/br/com/luisbrb/portifolio/springboot/controller/rest/SkillRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,20 @@ public class SkillRestController {
     @GetMapping("/all")
     public List<SkillEntity> getAll() {
         return skillRepository.findAll();
+    }
+
+    @PutMapping("/edit")
+    public SkillEntity edit(@CookieValue(name = Constants.AUTH_COOKIE) String authCookie, @RequestBody SkillEntity skillEntity) {
+        if (!AuthenticationUtils.isLoggedIn(authCookie)) {
+            return null;
+        };
+        
+        if (skillEntity.getId() == null) {
+            return null;
+        }
+        if (!skillRepository.existsById(skillEntity.getId())) {
+            return null;
+        }
+        return skillRepository.save(skillEntity);
     }
 }
