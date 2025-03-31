2a33bfec85d829075d69651d90761fb2e055ad1c
luis10barbo <luis10barbo2020@gmail.com>
2025-03-31 06:47:34 -0300

    Fix: Problem with no photo upload on skill creation

--- client/src/app/service/image/image-service.service.ts
+++ client/src/app/service/image/image-service.service.ts
@@ -13,17 +13,19 @@ export class ImageServiceService {
 
   public saveImages(images:({file: File, blob: string} | undefined)[]) {
     return new Observable<ImageType[] | undefined>((subscriber) => {
-      if (images.length < 1) {
-        subscriber.next(undefined);
-        return;
-      }
       const formData = new FormData();
+      let imgCount = 0;
       images.forEach((image) => {
         if (!image) {
           return;
         }
+        imgCount ++;
         formData.append("image", image.file);
       });
+      if (imgCount < 1) {
+        subscriber.next(undefined);
+        return;
+      }
       this.httpClient.post<ImageType[]>(`${enviroment.urlBackend}/image`, formData, {withCredentials: true}).subscribe((res) => {
         subscriber.next(res);
       });
